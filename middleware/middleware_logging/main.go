8ec1bb733378d90a5b97990824df830cb3ecb0f8
Guvanch Amanov <guvanch.amanov@skystart.team>
2023-12-31 03:42:07 +0300

    logging setted up

---
+++ middleware/middleware_logging/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", HomeHandler)
+	mux.HandleFunc("/foo", FooHandler)
+
+	middlewares := []func(http.Handler) http.Handler{
+		LoggingMiddleware,
+		SecondMiddleware,
+	}
+	handler := http.Handler(mux)
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		handler = middlewares[i](handler)
+	}
+
+	err := http.ListenAndServe(":3000", handler)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("home")
+	w.Write([]byte("Home"))
+}
+
+func FooHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("foo")
+	w.WriteHeader(400)
+	w.Write([]byte("Foo"))
+}
+
+func SecondMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("before 2")
+		handler.ServeHTTP(w, r)
+		fmt.Println("after 2")
+	})
+}
