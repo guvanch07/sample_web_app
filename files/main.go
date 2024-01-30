999da06e814bac091410b32cf50846e158841443
Guvanch Amanov <guvanch.amanov@skystart.team>
2024-01-30 10:43:43 +0300

    working with files

---
+++ files/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// начало решения
+
+// readLines возвращает все строки из указанного файла
+func readLines(name string) ([]string, error) {
+	file, err := os.Open(name)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось открыть файл: %v", err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("ошибка при чтении файла: %v", err)
+	}
+	if len(lines) > 0 && lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1]
+	}
+
+	return lines, nil
+}
+
+// конец решения
+
+// конец решения
+
+func main() {
+	lines, err := readLines("/etc/passwd")
+	if err != nil {
+		panic(err)
+	}
+	for idx, line := range lines {
+		fmt.Printf("%d: %s\n", idx+1, line)
+	}
+}
